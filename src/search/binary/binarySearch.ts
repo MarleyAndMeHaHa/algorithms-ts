1e5e7fc776ab3dc931882fb9bfa28b9cd119b0f7
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:26:45 +0000

    linear test done, started binary

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,6 +1,11 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
+    while (item=false){
+        if(items[(items.length-1)/2]=item){
+            item=true
+        }
+    }
 
     return NO_MATCH;
 }
